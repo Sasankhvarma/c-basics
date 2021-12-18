c4dacacd073150d2c7dbe652086241b1e33021fa
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2021-12-18 15:22:26 +0530

    Add files via upload

---
+++ a smal program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int     age;
+    float   perc;
+    char    initial;
+  
+  
+    age      =  17;
+    printf("Age is:%d\n",age);
+	perc     =  83.666745;
+    printf("Percentage is:%f\n",perc);
+	initial  =  'k';
+    printf("Surname starys with :%c\n",initial); 
+} 
