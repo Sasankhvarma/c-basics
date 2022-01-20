37106973bd3080ee4d27d4793ede7e333dbfc6cd
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-01-20 16:01:52 +0530

    Add files via upload

---
+++ noofprimenumbersinrange.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int i,s,c,a,b;
+	scanf("%d%d",&a,&b);
+	for(a;a<=b;a++)
+	{
+	   s=0;
+	   for(i=2;i<=a/2;i++)
+	    {
+		    if(a%i==0)
+			  {
+				s=1;
+				break;
+			  }
+        }
+        if(s==0 && a!=1)
+		  {
+			    printf("%d ",a);
+	      }
+    }
+}
